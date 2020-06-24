b94b9ce82177fa6496bfbee78cd61e23518668ce
245883541 <60998869+245883541@users.noreply.github.com>
2020-06-24 15:26:46 +0800

    1

--- ZhiShang/src/com/fire/controller/UserController.java
+++ ZhiShang/src/com/fire/controller/UserController.java
@@ -1,11 +1,37 @@
 package com.fire.controller;
 
+import java.io.File;
+
 import com.fire.model.Info;
 import com.jfinal.core.Controller;
+import com.jfinal.kit.PathKit;
 import com.jfinal.plugin.activerecord.Db;
+import com.jfinal.upload.UploadFile;
 
 public class UserController extends Controller{
+	
+	private static String rootPath = "";
+	static {
+		rootPath = PathKit.getWebRootPath()+"/img";
+	}
+	
+	public void uploadPic(){
+		UploadFile file = getFile();
+		File old = new File(rootPath,"idea.jpeg");
+		if(old.exists()) old.delete();
+		file.getFile().renameTo(new File(rootPath,"idea.jpeg"));
+		renderJson("num",1);
+		
+	}
 
+	public void getPic() {
+		File idea = new File(rootPath,"idea.jpeg");
+		if(idea.exists())
+			renderFile(idea);
+		else
+			renderFile(new File(rootPath,"nopic.png"));
+	}
+	
 	public void update() throws Exception{
     	Info info = getModel(Info.class,"u");
     	int num = 1;
