e61560800669a8abafe43bfb2f1fe9551b090b0b
245883541 <60998869+245883541@users.noreply.github.com>
2020-06-27 17:27:50 +0800

    1

--- ZhiShang/src/com/fire/controller/FIndexController.java
+++ ZhiShang/src/com/fire/controller/FIndexController.java
@@ -22,11 +22,11 @@ public class FIndexController extends Controller {
         //加载关于我们数据
         setAttr("info", Db.findFirst("select * from info_tb"));
         //加载证书数据
-        setAttr("books",Db.find("select * from book_tb where if_show=1 limit 4"));
+        setAttr("books",Db.find("select * from book_tb where if_show=1 order by get_time desc limit 4"));
         //加载案例数据
-        setAttr("cases",Db.find("select * from case_tb where if_show=1 limit 4"));
+        setAttr("cases",Db.find("select * from case_tb where if_show=1 order by ctime desc limit 4"));
         //加载新闻数据
-        setAttr("news",Db.find("select * from news_tb where if_show=1 limit 4"));
+        setAttr("news",Db.find("select * from news_tb where if_show=1 and type=0 order by time desc limit 4"));
         //加载轮播图
         setAttr("rcs",getIndexPics());
         setAttr("time",System.currentTimeMillis());
